4f629a885918823b971eaf187038177cf0cdb3b9
Carl Wallace (Mac Mojave VM) <carl@redhoundsoftware.com>
2020-07-29 07:16:51 -0400

    Revert "mods for iOS13/iPadOS13"
    This reverts commit 6082d079031ef0881e439d000ec9267c7434f468.

--- SampleKeyProviderExtensionFileProvider/FileProvider.h
+++ SampleKeyProviderExtensionFileProvider/FileProvider.h
@@ -4,14 +4,6 @@
 
 #import <UIKit/UIKit.h>
 
-#import <FileProvider/FileProvider.h>
-#import <MobileCoreServices/MobileCoreServices.h>
-
 @interface FileProvider : NSFileProviderExtension
 
-+ (BOOL) isZipIdentifier:(NSString*)identifier;
-+ (NSURL*) getZipPlaceholderUrl:(NSString*)identifier;
-+ (NSURL*) getP12PlaceholderUrl:(NSString*)identifier;
-
-@property (nonatomic, retain) NSString* password;
 @end
